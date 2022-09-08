99926a26360d699bfa0611101642c5fc3ffe7ad2
Yury Stolarz Santana <yurysscareer@gmail.com>
2022-09-08 15:56:38 -0300

    requisito 1 completo

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import reducerIncial from './reducerIncial';
+
+const rootReducer = combineReducers({
+  reducerIncial,
+});
+
+export default rootReducer;
---
+++ src/redux/reducers/reducerIncial.js
@@ -0,0 +1,9 @@
+const INITIAL_STATE = {};
+
+const reducerIncial = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  default: return state;
+  }
+};
+
+export default reducerIncial;
