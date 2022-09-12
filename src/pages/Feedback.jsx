d26af089ab73cb4cc37f70a5120345e38ad6e925
guigabriel <guilhermelisboa98@outlook.com>
2022-09-12 17:20:17 -0300

    Grupo 22 Requisito 15

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 
 class Feedback extends Component {
+  reedirectHome = () => {
+    const { history: { push } } = this.props;
+    push('/');
+  };
+
   render() {
     return (
       <div>
         <h1 data-testid="feedback-text">Feedback</h1>
+        <button
+          type="button"
+          data-testid="btn-play-again"
+          onClick={ this.reedirectHome }
+        >
+          Play Again
+
+        </button>
       </div>
     );
   }
